ed63f514d4c53b43a2eb2599eed837edd2b0cf40
Edeth3750 <edeth3125@gmail.com>
2024-05-02 13:10:19 +1000

    Greybox Hotair Balloon and Plane
    -Fade in/out cloud script
    -Removed duplicate folder

--- Assets/Scripts/CloudSpawner.cs
+++ Assets/Scripts/CloudSpawner.cs
@@ -19,6 +19,11 @@ public class CloudSpawner : MonoBehaviour
     [SerializeField] private int cloudHeight = 50;
     [SerializeField] private int boundaryWidth = 100;
     [SerializeField] private int boundaryLength = 100;
+
+    [Header("Fade Settings")]
+    [SerializeField] private float IncrementDelay = 0.2f;
+    [Range(0,1)]
+    [SerializeField] private float FadeIncrement = 0.05f;
     
     private GameObject CloudLayer;
     private float timer = 0f;
@@ -84,7 +89,45 @@ public class CloudSpawner : MonoBehaviour
                                                         cloudHeight, 
                                                         -boundaryWidth/2-Random.Range(0,spawnVariance));
             clouds.transform.parent = CloudLayer.transform;
+            StartCoroutine(FadeIn(clouds)); 
         }
+    }
+
+    IEnumerator FadeIn(GameObject clouds) 
+    {
+        for (float ft = 0f; ft <= 1; ft += FadeIncrement) 
+        {
+            for(int x = 0; x < clouds.transform.childCount; x++)
+            {
+                Color c = clouds.transform.GetChild(x).GetComponent<Renderer>().material.color; 
+                c.a = ft;
+                clouds.transform.GetChild(x).GetComponent<Renderer>().material.color = c;
+            }
+            yield return new WaitForSeconds(IncrementDelay);
+        }
+
+        //setting transparency to default after fade in
+        for(int x = 0; x < clouds.transform.childCount; x++)
+        {
+            Color c = clouds.transform.GetChild(x).GetComponent<Renderer>().material.color; 
+            c.a = 1f;
+            clouds.transform.GetChild(x).GetComponent<Renderer>().material.color = c;
+        }
+    }
+
+    IEnumerator FadeOut(GameObject clouds) 
+    {
+        for (float ft = 1f; ft >= 0; ft -= FadeIncrement) 
+        {
+            for(int x = 0; x < clouds.transform.childCount; x++)
+            {
+                Color c = clouds.transform.GetChild(x).GetComponent<Renderer>().material.color; 
+                c.a = ft;
+                clouds.transform.GetChild(x).GetComponent<Renderer>().material.color = c;
+            }
+            yield return new WaitForSeconds(IncrementDelay);
+        }
+        Destroy(clouds);
     }
 
     //Culls clouds that are too far, includes spawnVariance to avoid culling newly spawned clouds
@@ -98,7 +141,10 @@ public class CloudSpawner : MonoBehaviour
             cloud.transform.position.z > boundaryLength/2+spawnVariance ||
             cloud.transform.position.z < -boundaryLength/2-spawnVariance ) 
             {
-                Destroy(cloud);
+                if(cloud.transform.GetChild(0).GetComponent<Renderer>().material.color.a == 1f)
+                {
+                    StartCoroutine(FadeOut(cloud)); 
+                } 
             }
         }
     }
