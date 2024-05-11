eeda2436b22f25da284250c813b6338bef1514c8
Dylan W <dylanrwije@gmail.com>
2024-05-11 20:44:52 +1000

    Fireflies - Firefly Hover and Glow
    - Added Fireflies as part of the experience
    - Added a material for the firefly with an emissive yellow
    - Added point lights to draw a halo and lighting
    - Added prefabs for Firefly and FireflySpawner
    - Created a script specifically for the Firefly movement
    - Created a script for FireflySpawner to spawn fireflies in random positions

---
+++ Assets/Scripts/Firefly.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firefly : MonoBehaviour
+{
+    [Header("Firefly Movement")]
+    [SerializeField] private float fireflySpeed = 1f;
+    [SerializeField] private float fireflyAcceleration = 2f;
+    [SerializeField] private float initialMinFlyTimer = 0.5f;
+    [SerializeField] private float initialMaxFlyTimer = 2f;
+    [SerializeField] private float minFloatTimer = 1f;
+    [SerializeField] private float maxFloatTimer = 2.5f;
+    [SerializeField] private float ignoreSpeedLimitTimer = 0.2f;
+
+    private float initialTimer;
+    private float timer;
+    private float setTimer;
+    private float ignoreTimer;
+    private float direction;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialTimer = Random.Range(initialMinFlyTimer, initialMaxFlyTimer);
+        setTimer = Random.Range(minFloatTimer, maxFloatTimer);
+        timer = setTimer;
+        ignoreTimer = 0f;
+        direction = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // At initial state, have the fireflies float up for an added time before refering to its normal float time
+        if (initialTimer <= 0f)
+        {
+            // Wait before the timer runs out so that it reverses the hovering direction. Also reset ignore timers every time the normal timer runs out
+            if (timer <= 0f)
+            {
+                direction *= -1f;
+                timer = setTimer;
+                ignoreTimer = ignoreSpeedLimitTimer;
+            }
+            else
+            {
+                timer -= Time.deltaTime;
+                ignoreTimer -= Time.deltaTime;
+            }
+        }
+        else
+        {
+            initialTimer -= Time.deltaTime;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // The driving force of moving the firefly under a speed limit. Ignores speed limit whenever the ignore timer is active so that it can change directions
+        if (this.GetComponent<Rigidbody>().velocity.magnitude < fireflySpeed || ignoreTimer > 0f)
+        {
+            this.GetComponent<Rigidbody>().AddForce(new Vector3(0f, fireflyAcceleration * direction, 0f), ForceMode.Acceleration);
+        }
+    }
+}
---
+++ Assets/Scripts/FireflySpawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireflySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject fireflyPrefab;
+    [Header("Firefly Settings")]
+    [SerializeField] private int numberOfFireflies = 40;
+    [SerializeField] private float xAreaRadius = 40f;
+    [SerializeField] private float zAreaRadius = 40f;
+    [Range(0f, 100f)]
+    [SerializeField] private float raycastHeight = 30f;
+
+    RaycastHit hit;
+    int layerMasking = 1 << 0;
+
+    //Event start
+    public void Launch()
+    {
+        // Reveal them
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set itself to the origin while at a higher height
+        this.transform.position = new Vector3(0f, raycastHeight, 0f);
+        
+        // For how many number of fireflies, distribute them randomly among the area radius and paste them to the ground below
+        for (int i = 0; i < numberOfFireflies; i++)
+        {
+            GameObject firefly = Instantiate(fireflyPrefab, this.transform);
+            firefly.transform.localPosition = new Vector3(Random.Range(-xAreaRadius, xAreaRadius), 0f, Random.Range(-zAreaRadius, zAreaRadius));
+            if (Physics.Raycast(origin: firefly.transform.position, direction: Vector3.down, hitInfo: out hit, maxDistance: Mathf.Infinity, layerMask: layerMasking))
+            {
+                Debug.Log(hit.point.y);
+                firefly.transform.position = new Vector3(firefly.transform.position.x, hit.point.y, firefly.transform.position.z);
+            }
+        }
+
+        // Set them invisible for now
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+}
